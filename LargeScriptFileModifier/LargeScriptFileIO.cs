680e2130e669455146718311d9aae6bf69060a35
Twan <ti.twan@laposte.net>
2019-12-13 03:28:28 +0100

    Add project files.

---
+++ LargeScriptFileModifier/LargeScriptFileIO.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+namespace LargeScriptFileModifier
+{
+    public static class LargeScriptFileIO
+    {
+        internal static int Modify(string myFolder,
+            string myFileOrg, string myFileRev,
+            string oldString, string newString)
+        {
+            int cnt = 0;
+            //Task tk;
+            string filepathOrg = Path.Combine(myFolder, myFileOrg);
+            string filepathRev = Path.Combine(myFolder, myFileRev);
+
+            using (FileStream fs = File.Open(filepathOrg, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (BufferedStream bs = new BufferedStream(fs))
+                {
+                    using (StreamReader sr = new StreamReader(bs))
+                    {
+                        //using (FileStream fw = File.Create(filepathRev, 4096, FileOptions.WriteThrough))
+                        using (FileStream fw = new FileStream(filepathRev, FileMode.Create, FileAccess.ReadWrite))
+                        {
+                            using (StreamWriter sw = new StreamWriter(fw))
+                            {
+                                string lineOrg;
+                                string lineRev;
+                                while ((lineOrg = sr.ReadLine()) != null)
+                                {
+                                    if (lineOrg.Contains(oldString))
+                                    {
+                                        lineRev = lineOrg.Replace(oldString, newString,
+                                            StringComparison.InvariantCultureIgnoreCase);
+
+                                        cnt++;
+                                    }
+                                    else
+                                    {
+                                        lineRev = lineOrg;
+                                    }
+
+                                    sw.WriteLine(lineRev);
+                                    //tk = await sw.WriteLineAsync(lineRev);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            System.Diagnostics.Debug.Write("Number of catches = " + cnt);
+            //return tk;
+            return cnt;
+        }
+    }
+}
---
+++ LargeScriptFileModifier/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LargeScriptFileModifier
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Properties
+
+        /// <summary>
+        /// MyFolderPath
+        /// </summary>
+        public string MyFolderPath { get; set; } = Settings1.Default.FolderPath;
+
+        //public string MyFolderPath { get; set; } = @"C:\temp\";
+        //public string MyFolderPath { get; set; } = @"C:\Program Files (x86)\Adobe\Acrobat Reader DC\Resource\TypeSupport\Unicode\Mappings\win\";
+
+        /// <summary>
+        /// MyFileOrg
+        /// </summary>
+        public string MyFileOrg { get; set; } = Settings1.Default.FileOrg;
+
+        //public string MyFileOrg { get; set; } = @"MUP.xml";
+
+        /// <summary>
+        /// MyFileNew
+        /// </summary>
+        public string MyFileNew { get; set; } = Settings1.Default.FileNew;
+
+        //public string MyFileNew { get; set; } = @"Rewrote.xml";
+
+        /// <summary>
+        /// StringOld
+        /// </summary>
+        public string StringOld { get; set; } = Settings1.Default.StringOld;
+
+        //public string StringOld { get; set; } = @"deviceID";
+
+        /// <summary>
+        /// StringNew
+        /// </summary>
+        public string StringNew { get; set; } = Settings1.Default.StringNew;
+
+        //public string StringNew { get; set; } = @"systemUHD";
+
+        /// <summary>
+        /// ModificationCount
+        /// </summary>
+        public int ModificationCount { get; set; }
+
+        #endregion Properties
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void Button_start_Click(object sender, RoutedEventArgs e)
+        {
+            //Task.Factory.StartNew();
+            ModificationCount = LargeScriptFileIO.Modify(MyFolderPath,
+                MyFileOrg, MyFileNew,
+                StringOld, StringNew);
+            TextBlock_ModifCount.Text = ModificationCount.ToString();
+        }
+    }
+}
